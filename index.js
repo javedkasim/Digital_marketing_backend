e2880bfd8b8dbb3c3f00cad67d5bb268cd7142f3
javedkasim <javed78khan28@gmail.com>
2025-09-26 18:18:12 +0530

    seven commit

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ databaseConnection();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:3000",  // your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 app.get("/",(req, res)=>{
     res.send("Welcome Back Sir");
